6b12ee3446fa6aff5498976176ac5f6aa7840b0d
Sergey <sakovalchuk@tut.by>
2020-09-20 16:39:52 +0300

    buttons

--- src/components/MapFilter.vue
+++ src/components/MapFilter.vue
@@ -2,46 +2,47 @@
   <div id="map-filter">
     <div class="btn-toolbar" role="toolbar" aria-label="Toolbar">
       <div class="btn-group mr-2" role="group" aria-label="Find group">
-        <button v-for="(button, name, index) in buttons" type="button" :class="button.class" class="btn" :key="index" @click="buttonClick(name)">
-          <span class="material-icons">{{ button.icon }}</span>
+        <button v-for="(button, name, index) in buttons" type="button" :class="button.class" class="btn" :key="index"
+                @click="buttonClick(name)" :title="button.title">
+          <component v-bind:is="button.icon"  :title="button.title" />
         </button>
       </div>
     </div>
-    <div class="row mt-3">
+    <div class="row mt-3" v-if="buttons.roads.class == 'btn-success'">
       <div class="col-2">
         <select class="form-control" @change="highwaySelect">
           <option v-for="(highway, index) in highways" :key="index" :value="highway.id">{{ highway.name }}</option>
         </select>
       </div>
     </div>
-    <!--<div class="col-2">
-      <button class="btn btn-info" @click="buttonClick">Click</button>
-    </div>-->
 
   </div>
 </template>
 
 <script>
   import $ from "jquery";
-  import '@/assets/material-icons/material-icons.css'
+  import LocationEnter from 'vue-material-design-icons/LocationEnter';
+  import Road from 'vue-material-design-icons/Road';
 
   export default {
     name: 'MapFilter',
+    components: {
+      Road,
+      LocationEnter
+    },
     data: function () {
       return {
         highways: [],
         buttons: {
           roads: {
-            icon: 'timeline',
-            class: 'btn-success'
+            icon: 'road',
+            class: 'btn-success',
+            title: 'Поиск дороги'
           },
           address: {
-            icon: 'place',
-            class: 'btn-light'
-          },
-          points: {
-            icon: 'touch_app',
-            class: 'btn-light'
+            icon: 'location-enter',
+            class: 'btn-light',
+            title: 'Поиск адреса'
           },
 
         }
@@ -54,8 +55,9 @@
         })
       },
       buttonClick: function (source) {
-        console.log(source)
-        //this.$emit('buttonClick', {})
+        for (let button in this.buttons){
+          this.buttons[button].class = button == source ? 'btn-success' : 'button-light'
+        }
       }
     },
     mounted() {
